f194e4f09b13a6fec332e80d315544ec2f055639
Uvindu96 <uvindusri1996@gmail.com>
2019-09-13 11:15:00 +0530

    add login function

---
+++ loginfire.js
@@ -0,0 +1,16 @@
+function login(){
+  
+    var userEmail = document.getElementById("username").value;
+    var userPass = document.getElementById("password").value;
+  
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+  
+      window.alert("Error : " + errorMessage);
+  
+      // ...
+    });
+  
+  }
